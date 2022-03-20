da0a400a0a55dc5405b7b2bdf3ffc262777eff8f
AgustinTafura <agustintafura@hotmail.com>
2022-03-20 20:55:52 -0300

    DB store inputs validation

--- models/store.js
+++ models/store.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const StoreSchema = new mongoose.Schema({
-  name: String,
-  cuit: String,
-  concepts: Array,
-  currentBalance: Number,
-  active: Boolean,
-  lastSale: Date,
+  name : {type: String, required: true, lowercase: true, trim: true},
+  cuit : {type: Number, required: true, trim: true, minLength: 11, maxLength: 11},
+  concepts : {type: [String], required: true, lowercase: true},
+  currentBalance : {type: Number, required: true, trim: true},
+  active : {type: Boolean, required: true, trim: true},
+  lastSale : {type: Date, required: true, trim: true},
 },{ timestamps: true });
 
 StoreSchema.pre('save', async function (callback) {
